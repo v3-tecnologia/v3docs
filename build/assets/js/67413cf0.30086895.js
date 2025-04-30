291c25c955b5cd307f347fde00b7efdfc73cd7e3
Adriano Santos <adriano.santos@v3.com.br>
2025-04-30 01:31:19 -0300

    generate new doc version

--- build/assets/js/67413cf0.30086895.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkv_3_docs=self.webpackChunkv_3_docs||[]).push([[8003],{93651:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/v3docs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ build/assets/js/acecf23e.08d2955a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkv_3_docs=self.webpackChunkv_3_docs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
