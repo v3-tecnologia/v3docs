10a8e213811ddaab40ba21130b9d1d6999996b1c
Diogo Cezar <8019111+diogocezar@users.noreply.github.com>
2025-05-29 17:40:49 -0300

    chore: fix new

--- src/components/Headers.tsx
+++ src/components/Headers.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Title = ({ children }: { children: React.ReactNode }) => {
   return (
-    <h1 className="!text-primary !text-5xl/12 lg:!text-6xl/16 !font-bold ">
+    <h1 className="!text-primary text-3xl/10 md:!text-6xl/16 lg:!text-7xl/18 !font-bold ">
       {children}
     </h1>
   );
